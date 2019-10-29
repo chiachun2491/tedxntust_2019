b6ca45b128b99970959c8091bce2b7e8188ed6e0
Jeffery Ho <chiachun2491@gmail.com>
2019-10-29 15:16:57 +0800

    Update

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,40 @@
 $(document).ready(function () {
     $(function () {
 
-        $("a[href*='#']").click(function () {
+        $("a[href*='#']:not(a[href='#'])").click(function () {
 
+            $("#navbar").removeClass("show");
             var target = $(this.hash);
-
-            $("html,body").animate({
+            $("html, body").animate({
                 scrollTop: target.offset().top - 70
-            }, 500);
-
+            }, "slow");
             return false;
-
         });
 
+        $("a[href='#']").click(function () {
+
+            $("#navbar").removeClass("show");
+            $("html, body").animate({
+                scrollTop: 0
+            }, "slow");
+            return false;
+        });
     });
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() < $("#timetable").height()) {
+            $(".nav-item").removeClass("active");
+            $("#nav-speaker").addClass("active");
+        } else if ($(window).scrollTop() >= $("#timetable").height() && $(window).scrollTop() < $("#transport").height()) {
+            $(".nav-item").removeClass("active");
+            $("#nav-timetable").addClass("active");
+        } else if ($(window).scrollTop() >= $("#transport").height() && $(window).scrollTop() < $("#sponsor").height()) {
+            $(".nav-item").removeClass("active");
+            $("#nav-transport").addClass("active");
+        } else if ($(window).scrollTop() >= $("#sponsor").height()) {
+            $(".nav-item").removeClass("active");
+            $("#nav-sponsor").addClass("active");
+        }
+
+    })
 });
